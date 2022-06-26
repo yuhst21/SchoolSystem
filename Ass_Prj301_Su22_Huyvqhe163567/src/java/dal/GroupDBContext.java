49ff07b8f687494d587d9d88f4d6e6d3560f5a46
yuhst21 <mainluci1@gmail.com>
2022-06-27 01:09:16 +0700

    import dal

--- Ass_Prj301_Su22_Huyvqhe163567/src/java/dal/GroupDBContext.java
+++ Ass_Prj301_Su22_Huyvqhe163567/src/java/dal/GroupDBContext.java
@@ -4,18 +4,60 @@
  */
 package dal;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.Group;
+import model.Lecture;
+import model.Student;
+import model.Subject;
 
 /**
  *
  * @author win
  */
-public class GroupDBContext extends DBContext<Group>{
+public class GroupDBContext extends DBContext<Group> {
 
     @Override
     public ArrayList<Group> list() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+         ArrayList<Group> group = new ArrayList<>();
+        try {
+            String sql = "select g.gid,g.gname,g.lid,g.subjectid,s.subjectname,l.lname,st.sname,st.depid \n"
+                    + "from [Group] g inner join [Subject] s \n"
+                    + "on g.subjectid = s.subjectid\n"
+                    + "inner join Lecture l \n"
+                    + "on g.lid = l.lid\n"
+                    + "inner join Enroll e\n"
+                    + "on g.gid = e.gid\n"
+                    + "inner join Student st  \n"
+                    + "on e.sid = st.sid";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {                
+                Group g = new Group();
+                g.setGid(rs.getInt("gid"));
+                g.setGname(rs.getString("gname"));
+                Subject s = new Subject();
+                s.setSubjectid(rs.getInt("subjectid"));
+                s.setSubjectname(rs.getString("subjectname"));
+                g.setSub(s);
+                Lecture l = new Lecture();
+                l.setLid(rs.getInt("lid"));
+                l.setLname(rs.getString("lname"));
+                g.setLec(l);
+                Student stu = new Student();
+                stu.setSid(rs.getInt("sid"));
+                stu.setSname(rs.getString("sname"));
+                group.add(g);
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(GroupDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
 
     @Override
@@ -37,5 +79,5 @@ public class GroupDBContext extends DBContext<Group>{
     public void delete(Group entity) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
 }
