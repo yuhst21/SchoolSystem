5fed518482c0f201871be8b03467038afae49b17
yuhst21 <mainluci1@gmail.com>
2022-07-14 02:37:02 +0700

    Revert "Revert "update data""
    This reverts commit 6c78fa16fa5aaff1b8765d72a20da29011697ad4.

--- Ass_Prj301_Su22_Huyvqhe163567/src/java/controller/AttendController.java
+++ Ass_Prj301_Su22_Huyvqhe163567/src/java/controller/AttendController.java
@@ -2,55 +2,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
-import dal.GroupDBContext;
-import dal.LectureDBContext;
-import dal.SubjectDBContext;
+
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import model.Lecture;
-import model.Subject;
+
 
 /**
  *
  * @author win
  */
 public class AttendController extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AttendController</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AttendController at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    } 
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -58,26 +29,13 @@ public class AttendController extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-      SubjectDBContext sdb = new SubjectDBContext();
-        ArrayList<Subject> subject = sdb.list();
-        for (Subject s : subject) {
-             request.getSession().setAttribute("subject", sdb.get(s));
-             break;
-        }
-        LectureDBContext ldb = new LectureDBContext();
-        ArrayList<Lecture> lecture = ldb.list();
-        for (Lecture l : lecture) {
-             request.getSession().setAttribute("lecturer", ldb.get(l));
-           
-        }
-        GroupDBContext gdb = new GroupDBContext();
-        request.setAttribute("group", gdb.list());
-       request.getRequestDispatcher("/View/Attendance/attendance.jsp").forward(request, response);
-    } 
+            throws ServletException, IOException {
+       
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -85,13 +43,13 @@ public class AttendController extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         
-       
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
