3f3a7b89565d1b7f8b8764162d60a214970a19ea
yuhst21 <mainluci1@gmail.com>
2022-07-14 02:33:56 +0700

    Revert "Revert "fix data""
    This reverts commit 92cee8d743f603c706464713396f268edb4ffdc0.

--- Ass_Prj301_Su22_Huyvqhe163567/test/Test/Test.java
+++ Ass_Prj301_Su22_Huyvqhe163567/test/Test/Test.java
@@ -4,16 +4,6 @@
  */
 package Test;
 
-import controller.ScheduleController;
-import dal.LectureDBContext;
-import dal.SessionDBContext;
-import dal.SlotDBContext;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import model.Lecture;
-import model.Session;
-import model.Slot;
-import model.Week;
 
 /**
  *
@@ -21,17 +11,6 @@ import model.Week;
  */
 public class Test {
     public static void main(String[] args) {     
-        SessionDBContext sdb = new SessionDBContext();
-        ScheduleController s = new ScheduleController();
-        ArrayList<Week> weeks = s.getWeeksOfYear();
-        LocalDate currentDate = LocalDate.now();
-        Week currentWeek = s.getWeekByDate(weeks, currentDate);
-         Lecture lec = new Lecture();
-        lec.setLid(1);
-        ArrayList<Session> 
-         sessions = sdb.listSessionByLecture(lec, currentWeek.getStartDate(), currentWeek.getEndDate());
-        for (Session ses : sessions) {
-            System.out.println(ses.getClass());
-        }
+     
     }
 }
